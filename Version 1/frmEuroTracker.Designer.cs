7ac11235f91519858163fd01939fab8ed4b0bcb6
Unknown <pvanhalm00@gmail.com>
2017-12-20 21:48:42 +0100

    Added All Starter Files

---
+++ Version 1/frmEuroTracker.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Version_1
+{
+    partial class frmEuroTracker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(13, 13);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(174, 31);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Euro Tracker";
+            // 
+            // frmEuroTracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 423);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmEuroTracker";
+            this.Text = "EuroTracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ Version 1/frmEuroTracker.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+using MySql.Data;
+
+namespace Version_1
+{
+    public partial class frmEuroTracker : Form
+    {
+        public frmEuroTracker()
+        {
+            InitializeComponent();
+        }
+    }
+}
