74f8a3d5b89f7e7b0040af8955e61a598e19585a
Unknown <pvanhalm00@gmail.com>
2017-12-20 22:32:12 +0100

    Script Update

--- Version 1/frmEuroTracker.cs
+++ Version 1/frmEuroTracker.cs
@@ -1,10 +1,12 @@
 ﻿using System.Windows.Forms;
 using MySql.Data;
+using Ets2SdkClient;
 
 namespace Version_1
 {
     public partial class frmEuroTracker : Form
     {
+        Ets2SdkTelemetry telementry;
         public frmEuroTracker()
         {
             InitializeComponent();
