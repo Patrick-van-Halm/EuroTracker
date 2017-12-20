d66d4fd54450439d045b93add3737ad1c357303a
Unknown <pvanhalm00@gmail.com>
2017-12-20 23:28:18 +0100

    Update

--- Version 1/frmEuroTracker.cs
+++ Version 1/frmEuroTracker.cs
@@ -1,15 +1,51 @@
 ﻿using System.Windows.Forms;
 using MySql.Data;
 using Ets2SdkClient;
+using System;
 
 namespace Version_1
 {
     public partial class frmEuroTracker : Form
     {
         Ets2SdkTelemetry telementry;
+
         public frmEuroTracker()
         {
             InitializeComponent();
+
+            telementry = new Ets2SdkTelemetry();
+            telementry.Data += UpdateData;
+            telementry.JobFinished += TelemetryOnJobFinished;
+            telementry.JobStarted += TelemetryOnJobStarted;
+        }
+
+        private void TelemetryOnJobFinished(object sender, EventArgs args)
+        {
+            MessageBox.Show("Job finished, or at least unloaded nearby cargo destination.");
+        }
+
+        private void TelemetryOnJobStarted(object sender, EventArgs e)
+        {
+            MessageBox.Show("Just started job OR loaded game with active.");
+        }
+
+        private void UpdateData(Ets2Telemetry data, bool updated)
+        {
+            try
+            {
+                if (this.InvokeRequired)
+                {
+                    this.Invoke(new TelemetryData(UpdateData), new object[2] { data, updated });
+                    return;
+                }
+
+                float fuel = data.Drivetrain.Fuel;
+                lblFuel.Text = $"Fuel: {fuel}";
+            }
+            catch
+            {
+
+            }
         }
     }
 }
